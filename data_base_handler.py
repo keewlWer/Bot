d06e3a0e06ae8fe54223128a6c225629e1cf4dd8
keewlWer <92952187+keewlWer@users.noreply.github.com>
2024-01-27 22:24:01 +0200

    Update data_base_handler.py

--- data_base_handler.py
+++ data_base_handler.py
@@ -28,7 +28,20 @@ def save_to_db(user_data: list) -> None:
 def get_user(user_id: str, get_user_info=True) -> tuple or bool:
     conn = sqlite3.connect("main_database.db")
     c = conn.cursor()
-
+    c.execute(
+        """
+    CREATE TABLE IF NOT EXISTS users (
+    user_id TEXT PRIMARY KEY,
+    user_name TEXT,
+    age TEXT,
+    gender TEXT,
+    preferences TEXT,
+    user_info TEXT,
+    user_place TEXT,
+    image_link TEXT
+    )
+    """
+    )
     c.execute("SELECT * FROM users WHERE user_id=?", (user_id,))
 
     user_data = c.fetchone()
