b56453a7963b70e7da5b75b83dddc8d668a6b453
ThreeDiT1 <157525199+ThreeDiT1@users.noreply.github.com>
2024-01-30 10:22:43 +0100

    Add files via upload

--- data_base_handler.py
+++ data_base_handler.py
@@ -1,29 +1,34 @@
 import telebot
 import sqlite3
+from shared import user_data_dict
 
 
-def save_to_db(user_data: list) -> None:
-    conn = sqlite3.connect("main_database.db")
-    c = conn.cursor()
 
-    c.execute(
-        """
-    CREATE TABLE IF NOT EXISTS users (
-    user_id TEXT PRIMARY KEY,
-    user_name TEXT,
-    age TEXT,
-    gender TEXT,
-    preferences TEXT,
-    user_info TEXT,
-    user_place TEXT,
-    image_link TEXT
-    )
-    """
-    )
-    c.execute(""" INSERT OR REPLACE INTO users VALUES (?,?,?,?,?,?,?,?) """, user_data)
-    conn.commit()
-    conn.close()
+def save_to_db(user_id: int) -> None:
+    user_data = user_data_dict.get(user_id, [])
+    print (user_data)
 
+    if len(user_data) == 8:
+        conn = sqlite3.connect("main_database.db")
+        c = conn.cursor()
+        c.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                user_id TEXT PRIMARY KEY,
+                user_name TEXT,
+                age TEXT,
+                gender TEXT,
+                preferences TEXT,
+                user_info TEXT,
+                user_place TEXT,
+                image_link TEXT
+            )
+        """)
+
+        c.execute("INSERT OR REPLACE INTO users VALUES (?,?,?,?,?,?,?,?)", tuple(user_data))
+        conn.commit()
+        conn.close()
+    else:
+        print("Ошибка: user_data не содержит необходимое количество элементов")
 
 def get_user(user_id: str, get_user_info=True) -> tuple or bool:
     conn = sqlite3.connect("main_database.db")
@@ -55,11 +60,25 @@ def get_user(user_id: str, get_user_info=True) -> tuple or bool:
 
 
 def search_for_user(id: str) -> list:
+    x = 0
+    max_age_difference = 10  
     conn = sqlite3.connect("main_database.db")
     c = conn.cursor()
+
     c.execute("SELECT age from users WHERE user_id=?", (id,))
-    user_age = int(c.fetchone()[0])
-    c.execute("SELECT * from users WHERE age < ? AND age > ? AND user_id != ?", (user_age + 2, user_age - 2, id))
-    users_for_search = c.fetchall()
+    age_result = c.fetchone()
+    if age_result is None:
+        conn.close()
+        return []
+
+    user_age = int(age_result[0])
+    users_for_search = []
+
+    while not users_for_search and x <= max_age_difference:
+        c.execute("SELECT * from users WHERE age <= ? AND age >= ? AND user_id != ?", (user_age + x, user_age - x, id))
+        users_for_search = c.fetchall()
+        x += 1
+
     conn.close()
     return users_for_search
+
