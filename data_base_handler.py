0eacaf80b9f650a29a31468fd09537238d86df3b
deepwoodself <92952187+keewlWer@users.noreply.github.com>
2024-01-30 17:41:32 +0200

    Really bad idea

--- data_base_handler.py
+++ data_base_handler.py
@@ -3,15 +3,15 @@ import sqlite3
 from shared import user_data_dict
 
 
-
 def save_to_db(user_id: int) -> None:
     user_data = user_data_dict.get(user_id, [])
-    print (user_data)
+    print(user_data)
 
     if len(user_data) == 8:
         conn = sqlite3.connect("main_database.db")
         c = conn.cursor()
-        c.execute("""
+        c.execute(
+            """
             CREATE TABLE IF NOT EXISTS users (
                 user_id TEXT PRIMARY KEY,
                 user_name TEXT,
@@ -22,13 +22,17 @@ def save_to_db(user_id: int) -> None:
                 user_place TEXT,
                 image_link TEXT
             )
-        """)
+        """
+        )
 
-        c.execute("INSERT OR REPLACE INTO users VALUES (?,?,?,?,?,?,?,?)", tuple(user_data))
+        c.execute(
+            "INSERT OR REPLACE INTO users VALUES (?,?,?,?,?,?,?,?)", tuple(user_data)
+        )
         conn.commit()
         conn.close()
     else:
         print("Ошибка: user_data не содержит необходимое количество элементов")
+
 
 def get_user(user_id: str, get_user_info=True) -> tuple or bool:
     conn = sqlite3.connect("main_database.db")
@@ -60,25 +64,31 @@ def get_user(user_id: str, get_user_info=True) -> tuple or bool:
 
 
 def search_for_user(id: str) -> list:
-    x = 0
-    max_age_difference = 10  
+    x = 2
+    max_age_difference = 100
     conn = sqlite3.connect("main_database.db")
     c = conn.cursor()
 
-    c.execute("SELECT age from users WHERE user_id=?", (id,))
-    age_result = c.fetchone()
-    if age_result is None:
+    c.execute("SELECT age,preferences from users WHERE user_id=?", (id,))
+    result = c.fetchone()
+    if result is None:
         conn.close()
         return []
 
-    user_age = int(age_result[0])
+    user_age, preferences = int(result[0]), result[1]
     users_for_search = []
-
-    while not users_for_search and x <= max_age_difference:
-        c.execute("SELECT * from users WHERE age <= ? AND age >= ? AND user_id != ?", (user_age + x, user_age - x, id))
-        users_for_search = c.fetchall()
+    if preferences == 'Все':
+        preferences = ('Девушка', 'Парень')
+    while x <= max_age_difference:
+        for preference in (preferences, ):
+            print(preference)
+            c.execute(
+                "SELECT * from users WHERE age <= ? AND age >= ? AND user_id != ? AND gender = ?",
+                (user_age + x, user_age - x, id, preference),
+            )
+            new_users = c.fetchall()
+            users_for_search.extend(new_users)
         x += 1
-
+        print(x)
     conn.close()
     return users_for_search
-
